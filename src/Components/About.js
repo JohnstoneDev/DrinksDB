e05ad81e82289cd8c6ca1068e6f638903590ff88
Johnstone Marucha <marucha.webdev@yahoo.com>
2022-10-10 10:35:15 +0300

    update name

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,7 +2,7 @@ function About(){
    return (
     <div className="h-screen w-screen py-12 px-12">
         <h2 className="py-2.5 px-2.5">
-            Cocktails DB finds you the best cocktails and their Ingredients.
+            Drinks DB finds you the best cocktails and their Ingredients.
             To search for a drink, input the drink name and click the criteria button,then wait for the magic! 
             You can save them to your Collection and prepare them later when you need to enjoy! 
         </h2>
